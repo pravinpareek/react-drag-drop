efae1975654e83a5f7952319c6e10e92cc33ae55
Pravin Pareek <pareek.pravin@gmail.com>
2020-11-12 17:10:20 +0530

    Simple drag and drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, DragEvent } from 'react';
 import './App.css';
 
+interface ITask {
+  statusId: number
+  taskName: string
+  taskId: string
+}
+
 function App() {
+  const [status, updateStatus] = useState(['TODO', 'IN PROGRESS'])
+  const [tasks, updateTask] = useState<Array<ITask>>([
+    {
+      statusId: 0,
+      taskName: 'UX Design',
+      taskId: '0'
+    },
+    {
+      statusId: 0,
+      taskName: 'Service Setup',
+      taskId: '1'
+    },
+    {
+      statusId: 1,
+      taskName: 'UI Setup',
+      taskId: '2'
+    }
+  ])
+
+  const getTaskByStatus = (statusId: number) => {
+    return tasks.filter(task => task.statusId === statusId)
+  }
+
+  const onDrop = (e: DragEvent) => {
+    e.preventDefault()
+    e.stopPropagation()
+    // console.log('E', e)
+    const taskId = e.dataTransfer.getData('id')
+    const target: any = e.target
+    const statusId = target.id
+    tasks[+taskId].statusId = +statusId
+    updateTask([...tasks])
+  }
+
+  const onDragStart = (e: DragEvent) => {
+    // e.preventDefault()
+    // e.stopPropagation()
+    // console.log('E', e)
+    const target: any = e.target
+    e.dataTransfer.setData('id', target.id)
+  }
+
+  const onDragOver = (e: DragEvent) => {
+    e.preventDefault()
+    e.stopPropagation()
+    // console.log('E', e)
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <ul>
+        {status.map((s, i) => <li key={i}>
+          <div className='statusHeader'>
+            {s}
+          </div>
+          <div className='taskWrapper' onDrop={onDrop} onDragOver={onDragOver} id={'' + i}>
+            {
+              getTaskByStatus(i).map((task, taskIndex) => <div className='taskBox' key={taskIndex}
+                draggable={true} onDragStart={onDragStart} id={task.taskId}>
+                {task.taskName}
+              </div>)
+            }
+          </div>
+        </li>)}
+      </ul>
+
     </div>
   );
 }
